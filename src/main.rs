97272cc01f7eb10f2eae7d2b87048df88c6a95b1
Delta <riku@kawaii.sh>
2020-09-09 11:09:14 -0500

    Remove actix and add runnables

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,32 @@ fn main() {
     v8::V8::initialize_platform(platform);
     v8::V8::initialize();
 
-    let result = runnable::run(String::from("console.log('gg')"));
+    //let mut isolate = v8::Isolate::new(v8::CreateParams::default().heap_limits(0, 10000));
+    let mut iso = runnable::default_isolate();
+    let mut worker = runnable::Worker::new(&mut iso);
+
+    worker.globals.push(runnable::Global{
+        key: v8::Local::from(v8::String::new(&mut worker.scope, "owo").unwrap()), 
+        value: v8::Local::from(v8::String::new(&mut worker.scope, "awa").unwrap())
+    });
+
+    let result = worker.run(String::from("
+    const x = (num1, num2) => {
+        return num1 + num2
+    }
+
+    //x(6, 4)
+    owo
+    "));
+
+    // let result = runnable::run(String::from("
+    // const x = (num1, num2) => {
+    //     return num1 + num2
+    // }
+
+    // //x(6, 4)
+    // owo
+    // "));
 
     println!("{}", result)
 }
--- src/runnable.rs
+++ src/runnable.rs
@@ -1,14 +1,39 @@
 use rusty_v8 as v8;
 
-pub fn run(code: String) -> String {
-    let isolate = &mut v8::Isolate::new(v8::CreateParams::default().heap_limits(0, 10000));
-    
-    let scope = &mut v8::HandleScope::new(isolate);
-    let context = v8::Context::new(scope);
-    let scope = &mut v8::ContextScope::new(scope, context);
+pub struct Worker<'a> {
+    pub globals: Vec<Global<'a>>,
+    pub scope: v8::HandleScope<'a, ()>,
+}
 
-    let code = v8::String::new(scope, &code[..]).unwrap();
-    let result = v8::Script::compile(scope, code, None).unwrap().run(scope).unwrap().to_string(scope).unwrap();
+pub struct Global<'a> {
+    pub key: v8::Local<'a, v8::Name>,
+    pub value: v8::Local<'a, v8::Data>,
+}
 
-    result.to_rust_string_lossy(scope)
+pub fn default_isolate() -> v8::OwnedIsolate {
+    v8::Isolate::new(v8::CreateParams::default().heap_limits(0, 10000))
+}
+
+impl<'a> Worker<'a> {
+    pub fn new(isolate: &'a mut v8::OwnedIsolate) -> Self {
+        Worker {
+            globals: Vec::new(),
+            scope: v8::HandleScope::new(isolate),
+        }
+    }
+    pub fn run(&mut self, code: String) -> String {
+        let global = v8::ObjectTemplate::new(&mut self.scope);
+
+        for g in self.globals.iter() {
+            global.set(g.key, g.value);
+        }
+     
+        let context = v8::Context::new_from_template(&mut self.scope, global);
+        let scope = &mut v8::ContextScope::new(&mut self.scope, context);
+
+        let code = v8::String::new(scope, &code[..]).unwrap();
+        let result = v8::Script::compile(scope, code, None).unwrap().run(scope).unwrap().to_string(scope).unwrap();
+
+        result.to_rust_string_lossy(scope)
+    }
 }
