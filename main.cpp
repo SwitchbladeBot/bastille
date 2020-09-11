2cb87ebc8979d08f9b8451d2dffe688760310f3c
Delta <riku@kawaii.sh>
2020-09-10 20:32:49 -0500

    Modules

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
     v8scope v8(argv[0]);
 
     auto runnable = std::make_unique<Runnable>();
-    std::cout << runnable->Run("5+5");
+    std::cout << runnable->Run("exo.pi");
 
     return 0;
 }
--- src/runtime/runnable.cpp
+++ src/runtime/runnable.cpp
@@ -4,6 +4,8 @@
 #include <libplatform/libplatform.h>
 #include <string>
 
+#include <v8pp/module.hpp>
+
 Runnable::Runnable() {
     v8::Isolate::CreateParams create_params;
     this->allocator = create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
@@ -13,6 +15,14 @@ std::string Runnable::Run(const std::string& code) {
     v8::HandleScope handle_scope(isolate);
     v8::Local<v8::Context> context = v8::Context::New(isolate);
     v8::Context::Scope context_scope(context);
+
+    v8pp::module test(isolate);
+    test.set("string", v8::Local<v8::Value>(v8::String::NewFromUtf8(isolate, "test").ToLocalChecked()));
+    test.set("number", v8::Local<v8::Value>(v8::Number::New(isolate, 0.69)));
+
+    isolate->GetCurrentContext()->Global()->Set(context,
+            v8::String::NewFromUtf8(isolate, "test").ToLocalChecked(),
+            test.new_instance());
 
     v8::MaybeLocal<v8::String> code_local = v8::String::NewFromUtf8(isolate, code.c_str());
     v8::MaybeLocal<v8::Script> code_compiled = v8::Script::Compile(context, code_local.ToLocalChecked());
